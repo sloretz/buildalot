4d4904d959f6d99087f25510cdb84eb576f708d0
Shane Loretz <shane.loretz@gmail.com>
2024-04-22 07:25:59 -0700

    buildahlot -> buildalot
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

--- example_cli.sh
+++ example_cli.sh
@@ -1,1 +1,1 @@
-buildahlot --dry-run --one-arch --config example_config.yaml --arg=registry=localhost rolling
+buildalot --dry-run --one-arch --config example_config.yaml --arg=registry=localhost rolling
