1ec6f903d1639f2b3e1f51ff1a7c9f62a85280d8
Shane Loretz <shane.loretz@gmail.com>
2024-05-02 21:36:57 -0700

    Many cohesive output test
    Signed-off-by: Shane Loretz <shane.loretz@gmail.com>

--- buildalot/tests/cohesive_output_test.py
+++ buildalot/tests/cohesive_output_test.py
@@ -1,3 +1,6 @@
+import contextlib
+import random
+
 from buildalot.cohesive_output import CohesiveOutput
 
 
@@ -21,8 +24,6 @@ def test_one_output(capsys):
 
 
 def test_nested_output(capsys):
-    name = "foobar"
-    message = "Hello world!"
     with CohesiveOutput("co1") as co1:
         co1.write("co1: foo\n")
         with CohesiveOutput("co2") as co2:
@@ -43,3 +44,34 @@ def test_nested_output(capsys):
         "co2: bar",
         "<<< End output from: co2",
     ]
+
+
+def test_many_outputs(capsys):
+    outputs = {}
+    for i in range(100):
+        outputs[i] = CohesiveOutput(f"co{i}")
+
+    with contextlib.ExitStack() as exit_stack:
+        for co in outputs.values():
+            exit_stack.enter_context(co)
+
+        # Average 10 messages from each CohesiveOutput
+        for m in range(len(outputs) * 10):
+            i, co = random.choice(tuple(outputs.items()))
+            co.write(f"co{i}: {m}\n")
+
+    captured = capsys.readouterr()
+    stdout_lines = captured.out.split("\n")[:-1]  # -1 to remove '' from last \n
+
+    i = 0
+    started = False
+    for line in stdout_lines:
+        co = outputs[i]
+        if not started:
+            assert line == f">>> Begin output from: co{i}", stdout_lines
+            started = True
+        elif line == f"<<< End output from: co{i}":
+            started = False
+            i += 1
+        else:
+            assert line.startswith(f"co{i}"), stdout_lines
